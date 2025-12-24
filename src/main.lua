29836c901b25eed85d43252e32dacbdbcb0e5495
Rodrigo A. do Amaral <rodrigo.zfc@gmail.com>
2025-12-24 18:17:36 -0300

    initial commit

---
+++ src/main.lua
@@ -0,0 +1,11 @@
+local argparse = require("argparse")
+local inspect = require("inspect")
+local cjson = require("cjson")
+
+return {
+    name = 'luxdummy2'
+    _VERSION = "0.1.0",
+    argparse = argparse,
+    inspect = inspect,
+    cjson = cjson,
+}
